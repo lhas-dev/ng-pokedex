ac36435fe30e4805cc31a7abbaf85a8000dccd6d
Luiz Almeida <luizhrqas@gmail.com>
2025-01-23 22:52:33 -0300

    add 404 route

---
+++ apps/frontend/src/app/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { HeaderComponent } from "../shared/header/header.component";
+
+@Component({
+  selector: "app-not-found",
+  imports: [HeaderComponent],
+  template: `
+    <app-header />
+    <div class="container mx-auto">
+      <h1 class="text-2xl font-bold">Ops...</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  `,
+  styles: ``,
+})
+export class NotFoundComponent {}
--- apps/frontend/src/app/shared/pokedex-list/pokedex-list.component.ts
+++ apps/frontend/src/app/shared/pokedex-list/pokedex-list.component.ts
@@ -1,7 +1,30 @@
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+interface Pokedex {
+  id: number;
+  name: string;
+  pokemons: string[];
+}
 
 @Component({
   selector: "app-pokedex-list",
   templateUrl: "./pokedex-list.template.html",
+  standalone: true,
+  imports: [RouterModule, CommonModule],
 })
-export class PokedexListComponent {}
+export class PokedexListComponent {
+  pokedexes: Pokedex[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Pokedex[]>("http://localhost:3000/pokedex")
+      .subscribe((response) => {
+        this.pokedexes = response;
+      });
+  }
+}
