3572b81f4501f89d8d1bb3178c07f05943c6b894
Luiz Almeida <luizhrqas@gmail.com>
2025-01-24 00:15:16 -0300

    add search for pokemons

--- apps/frontend/src/app/routes/add-pokedex/add-pokedex.component.ts
+++ apps/frontend/src/app/routes/add-pokedex/add-pokedex.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { Pokedex } from "@frontend/typings";
 import {
   FormArray,
   FormBuilder,
@@ -11,6 +12,10 @@ import {
 import { HeaderComponent } from "../../shared/header/header.component";
 import { CommonModule } from "@angular/common";
 import { Pokemon } from "@frontend/typings";
+import { Router } from "@angular/router";
+import { map } from "rxjs/operators";
+import { switchMap } from "rxjs/operators";
+import { of, Subscription, timer } from "rxjs";
 
 @Component({
   selector: "app-add-pokedex",
@@ -23,10 +28,11 @@ export class AddPokedexComponent {
   pokemons: Pokemon[] = [];
   limit = 15;
   offset = 0;
-
+  searchSubscription: Subscription | null = null;
   constructor(
     private formBuilder: FormBuilder,
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) {
     this.form = this.formBuilder.group({
       name: ["", [Validators.required, Validators.minLength(2)]],
@@ -79,9 +85,52 @@ export class AddPokedexComponent {
     }
   }
 
+  onSearch(event: any) {
+    const searchTerm = event.target.value;
+    this.pokemons = [];
+    this.offset = 0;
+
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+
+    this.searchSubscription = timer(300)
+      .pipe(
+        switchMap(() => {
+          if (!searchTerm) {
+            this.getPokemons();
+            return of({ results: [] });
+          }
+          return this.http
+            .get<{
+              results: Pokemon[];
+            }>(`https://pokeapi.co/api/v2/pokemon?limit=1500`)
+            .pipe(
+              map((response) => ({
+                results: response.results.filter((pokemon) =>
+                  pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+                ),
+              }))
+            );
+        })
+      )
+      .subscribe((pokemons) => {
+        console.log(pokemons);
+        this.pokemons = pokemons.results;
+      });
+  }
+
   onSubmit() {
     if (this.form.valid) {
-      console.log("Form Submitted!", this.form.value);
+      this.http
+        .post<Pokedex>("http://localhost:3000/pokedex", {
+          name: this.form.value.name,
+          pokemons: this.selectedPokemons.value,
+        })
+        .subscribe((response) => {
+          this.router.navigate(["/pokedex", response.id]);
+          console.log("Form Submitted!", response);
+        });
     }
   }
 }
